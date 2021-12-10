6b0f98980277408401635af932b3c23037c38ec9
c-vast <hechenghao1998@foxmail.com>
2021-12-10 17:00:54 +0800

    2021.12.10

---
+++ vast-common/src/main/java/com/vast/common/component/JWTOperator.java
@@ -0,0 +1,24 @@
+package com.vast.common.component;
+
+import com.vast.common.config.properties.TokenProperties;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JWTOperator {
+
+    @Autowired
+    private TokenProperties tokenProperties;
+
+    public String create(){
+        return null;
+    }
+
+    public void parse(){
+
+    }
+
+    public void verify(){
+
+    }
+}
