17c29b77f46388a7f46a38358df873545cea4185
cvast <hechenghao1998@foxmail.com>
2023-10-13 21:50:12 +0800

    框架配置问题

--- vast-common/src/main/java/com/vast/common/cassandra/CassandraDao.java
+++
@@ -1,8 +0,0 @@
-package com.vast.common.cassandra;
-
-import org.springframework.data.cassandra.repository.CassandraRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CassandraDao<T,ID> extends CassandraRepository<T,ID> {
-}
